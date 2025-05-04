7b9f31119b9f02882827f246c2cd4f9f67d6f74c
Sandora <sandronizharadze77@gmail.com>
2025-05-04 15:24:51 +0400

    3

--- jobsy/wsgi.py
+++ jobsy/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Use production settings if RENDER environment variable is set
+# Use render_settings.py if RENDER environment variable is set
 if os.environ.get('RENDER'):
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobsy.production_settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobsy.render_settings')
 else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobsy.settings')
 
