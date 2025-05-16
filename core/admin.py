2730cb2827219016895a4ac29bda07988b8643c6
Sandora <sandronizharadze77@gmail.com>
2025-05-16 22:01:14 +0400

    Add import/export functionality for job listings

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import JobListing, UserProfile, EmployerProfile
+from .models import JobListing, UserProfile, EmployerProfile, JobApplication
+from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin
+from import_export import resources
+from rangefilter.filters import DateRangeFilter
+
+# Resources for model export
+class JobListingResource(resources.ModelResource):
+    class Meta:
+        model = JobListing
+        fields = ('id', 'title', 'company', 'description', 'salary_min', 'salary_max', 
+                  'salary_type', 'category', 'location', 'posted_at', 'experience',
+                  'job_preferences', 'considers_students', 'status', 'premium_level')
+
+class EmployerProfileResource(resources.ModelResource):
+    class Meta:
+        model = EmployerProfile
+        fields = ('id', 'company_name', 'company_id', 'phone_number', 'company_website', 
+                 'company_description', 'company_size', 'industry', 'location', 
+                 'user_profile__user__email', 'created_at')
+
+class JobApplicationResource(resources.ModelResource):
+    class Meta:
+        model = JobApplication
+        fields = ('id', 'job__title', 'job__company', 'user__email', 'guest_name', 
+                 'guest_email', 'status', 'applied_at')
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
@@ -9,7 +33,7 @@ class UserProfileInline(admin.StackedInline):
     verbose_name_plural = 'User Profile'
     fields = ('role', 'profile_picture')
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(UserAdmin, ImportExportActionModelAdmin):
     inlines = (UserProfileInline,)
     list_display = ('email', 'first_name', 'last_name', 'get_role', 'get_company')
     actions = ['make_employer']
@@ -55,7 +79,8 @@ admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
 @admin.register(EmployerProfile)
-class EmployerProfileAdmin(admin.ModelAdmin):
+class EmployerProfileAdmin(ImportExportModelAdmin):
+    resource_class = EmployerProfileResource
     list_display = ('company_name', 'get_employer_email', 'industry', 'company_size', 'location')
     search_fields = ('company_name', 'user_profile__user__email')
     list_filter = ('company_size', 'industry')
@@ -65,9 +90,10 @@ class EmployerProfileAdmin(admin.ModelAdmin):
     get_employer_email.short_description = 'Employer Email'
 
 @admin.register(JobListing)
-class JobListingAdmin(admin.ModelAdmin):
+class JobListingAdmin(ImportExportModelAdmin):
+    resource_class = JobListingResource
     list_display = ('title', 'company', 'get_employer', 'salary_range', 'location', 'premium_level', 'posted_at')
-    list_filter = ('employer__company_name', 'location', 'premium_level')
+    list_filter = (('posted_at', DateRangeFilter), 'employer__company_name', 'location', 'premium_level', 'status')
     search_fields = ('title', 'company', 'description', 'location')
     date_hierarchy = 'posted_at'
 
@@ -86,3 +112,25 @@ class JobListingAdmin(admin.ModelAdmin):
             return obj.employer.user_profile.user.email
         return '-'
     get_employer.short_description = 'Posted by'
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(ImportExportModelAdmin):
+    resource_class = JobApplicationResource
+    list_display = ('get_job_title', 'get_company', 'get_applicant', 'status', 'applied_at')
+    list_filter = (('applied_at', DateRangeFilter), 'status', 'job__title', 'job__company')
+    search_fields = ('job__title', 'job__company', 'user__email', 'guest_name', 'guest_email')
+    date_hierarchy = 'applied_at'
+    
+    def get_job_title(self, obj):
+        return obj.job.title
+    get_job_title.short_description = 'Job Title'
+    
+    def get_company(self, obj):
+        return obj.job.company
+    get_company.short_description = 'Company'
+    
+    def get_applicant(self, obj):
+        if obj.user:
+            return obj.user.email
+        return f"{obj.guest_name} ({obj.guest_email})"
+    get_applicant.short_description = 'Applicant'
--- jobsy/settings.py
+++ jobsy/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'import_export',
+    'rangefilter',
     'core',
 ]
 
